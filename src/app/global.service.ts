0cb8898e49b63186d6930fe494d6cb00612c7fd4
MichalSalek <contact@michalsalek.pl>
2019-01-17 00:59:08 +0100

    test method deleted (clean up)

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -16,16 +16,10 @@ export class GlobalService implements OnInit {
   apiParams = '&orientation=landscape';
   accesKey = '?client_id=f028e3c9ede7246105782861bb2d89788d2903daec5db2bfc62d2be6afb790f3';
 
-
   randomPhoto(): Observable<OnePhoto> {
     return this.http.get<OnePhoto>(`${this.apiUrl}/photos/random${this.accesKey}${this.apiParams}`);
   }
 
-  test() {
-    console.log(`${this.apiUrl}/photos/random${this.accesKey}${this.apiParams}`);
-  }
-
   ngOnInit() {
-    this.test();
   }
 }
